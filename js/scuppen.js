8697fe071e223259d639b46a1a48a00616759137
Sean Scully <sean.erik.scully@gmail.com>
2019-02-03 13:22:15 +0100

    Forberedelser for scuppen 2019 + resultater etter første runde

--- js/scuppen.js
+++ js/scuppen.js
@@ -2,25 +2,30 @@ const PARTICIPATED = -1,
     NOT_PARTICIPATED = -2;
 
 var participants = [
-    {name: 'Ole Tommy', results: [PARTICIPATED, 6, PARTICIPATED, PARTICIPATED, PARTICIPATED, 1]},
-    {name: 'Ivar', results: [PARTICIPATED, NOT_PARTICIPATED, NOT_PARTICIPATED, NOT_PARTICIPATED, 1, NOT_PARTICIPATED]},
-    {name: 'Jan Erik', results: [PARTICIPATED, 7, PARTICIPATED, NOT_PARTICIPATED, PARTICIPATED, PARTICIPATED]},
-    {name: 'Herman', results: [PARTICIPATED, 6, PARTICIPATED, 3, PARTICIPATED, NOT_PARTICIPATED]},
-    {name: 'Ole-André', results: [4, 6, PARTICIPATED, PARTICIPATED, NOT_PARTICIPATED, NOT_PARTICIPATED]},
-    {name: 'Tor Eric', results: [PARTICIPATED, 6, PARTICIPATED, 1, PARTICIPATED, PARTICIPATED]},
-    {name: 'Håken', results: [PARTICIPATED, 4, PARTICIPATED, PARTICIPATED, 4, 4]},
-    {name: 'Gustav', results: [NOT_PARTICIPATED, 2, PARTICIPATED, PARTICIPATED, PARTICIPATED, PARTICIPATED]},
-    {name: 'Erlend', results: [1, 7, 1, PARTICIPATED, NOT_PARTICIPATED, PARTICIPATED]},
-    {name: 'Marius', results: [PARTICIPATED, 7, PARTICIPATED, 2, PARTICIPATED, PARTICIPATED]},
-    {name: 'Erik', results: [NOT_PARTICIPATED, 3, PARTICIPATED, 4, PARTICIPATED, 2]},
-    {name: 'Richard', results: [PARTICIPATED, 3, 1, PARTICIPATED, PARTICIPATED, PARTICIPATED]},
-    {name: 'Lars Fredrik', results: [NOT_PARTICIPATED, 6, PARTICIPATED, PARTICIPATED, NOT_PARTICIPATED, NOT_PARTICIPATED]},
-    {name: 'Fredrik B', results: [PARTICIPATED, 4, 1, NOT_PARTICIPATED, NOT_PARTICIPATED, PARTICIPATED]},
-    {name: 'Fredrik S', results: [PARTICIPATED, 4, PARTICIPATED, PARTICIPATED, 3, NOT_PARTICIPATED]},
-    {name: 'Bjarte', results: [PARTICIPATED, 7, PARTICIPATED, PARTICIPATED, 2, NOT_PARTICIPATED]},
-    {name: 'Vetle', results: [PARTICIPATED, 1, PARTICIPATED, PARTICIPATED, PARTICIPATED, 4]},
-    {name: 'Ken', results: [3, 5, 1, PARTICIPATED, PARTICIPATED, PARTICIPATED]},
-    {name: 'Sean', results: [2, NOT_PARTICIPATED, NOT_PARTICIPATED, PARTICIPATED, PARTICIPATED, PARTICIPATED]}
+    {name: 'Ole Tommy', results: [PARTICIPATED]},
+    {name: 'Ivar', results: [PARTICIPATED]},
+    {name: 'Jan Erik', results: [PARTICIPATED]},
+    {name: 'Herman', results: [NOT_PARTICIPATED]},
+    {name: 'Ole-André', results: [PARTICIPATED]},
+    {name: 'Tor Eric', results: [PARTICIPATED]},
+    {name: 'Håken', results: [PARTICIPATED]},
+    {name: 'Gustav', results: [PARTICIPATED]},
+    {name: 'Erlend', results: [NOT_PARTICIPATED]},
+    {name: 'Marius', results: [PARTICIPATED]},
+    {name: 'Erik', results: [PARTICIPATED]},
+    {name: 'Richard', results: [NOT_PARTICIPATED]},
+    {name: 'Lars Fredrik', results: [PARTICIPATED]},
+    {name: 'Fredrik B', results: [4]},
+    {name: 'Fredrik S', results: [NOT_PARTICIPATED]},
+    {name: 'Vetle', results: [PARTICIPATED]},
+    {name: 'Ken', results: [NOT_PARTICIPATED]},
+    {name: 'Seán', results: [2]},
+    {name: 'Lars Olav', results: [1]},
+    {name: 'Terje', results: [NOT_PARTICIPATED]},
+    {name: 'Jan Tore', results: [3]},
+    {name: 'Jarle', results: [PARTICIPATED]},
+    {name: 'Ann Katrin', results: [NOT_PARTICIPATED]},
+    {name: 'Kristoffer', results: [NOT_PARTICIPATED]},
 ];
 
 var participantsTemplate = _.template(
@@ -28,14 +33,7 @@ var participantsTemplate = _.template(
 );
 
 var rounds = [
-    {head: "PingPong", points: [500, 400, 300, 200], participationScore: 100},
-    {head: "BONUSRUNDE: Vinsmaking", points: [150, 120, 100, 80, 50, 30, 0], participationScore: 0},
-    {head: "Kokkekamp", points: [300], participationScore: 100},
-    {head: "HatTrick", points: [500, 400, 300, 200], participationScore: 100},
-    {head: "Hafjell Poker", points: [250, 200, 150, 100], participationScore: 0},
-    {head: "Fekting", points: [550, 450, 0, 300], participationScore: 100}, // delt 4. plass
-    {head: "Quiz", points: [450, 350, 250], participationScore: 100},
-    {head: "SCIPT", points: [250, 200, 150, 100], participationScore: 50}
+    {head: "Lynsjakk", points: [500, 400, 300, 200], participationScore: 100},
 ];
 
 
@@ -77,7 +75,11 @@ function calcScoreDeductTwoWorst(participant) {
     // participation = -1, non-participation = -2
 
     var totalScore = 0;
-    _.each(participant.scoreValues.slice(0).sort(compareNumbers).slice(2), function(result, i) {
+    var scoreValuesList = participant.scoreValues.length >= 2
+      ? participant.scoreValues.slice(0).sort(compareNumbers).slice(2)
+      : participant.scoreValues;
+
+    _.each(scoreValuesList, function(result, i) {
         totalScore += result;
     });
 
