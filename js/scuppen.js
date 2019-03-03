15cb18a847e1e31c86578d09c48eaea85d16a709
Sean Scully <sean.erik.scully@gmail.com>
2019-03-03 22:36:32 +0100

    Oppdatert scuptabell etter 2. runde

--- js/scuppen.js
+++ js/scuppen.js
@@ -2,30 +2,30 @@ const PARTICIPATED = -1,
     NOT_PARTICIPATED = -2;
 
 var participants = [
-    {name: 'Ole Tommy', results: [PARTICIPATED]},
-    {name: 'Ivar', results: [PARTICIPATED]},
-    {name: 'Jan Erik', results: [PARTICIPATED]},
-    {name: 'Herman', results: [NOT_PARTICIPATED]},
-    {name: 'Ole-André', results: [PARTICIPATED]},
-    {name: 'Tor Eric', results: [PARTICIPATED]},
-    {name: 'Håken', results: [PARTICIPATED]},
-    {name: 'Gustav', results: [PARTICIPATED]},
-    {name: 'Erlend', results: [NOT_PARTICIPATED]},
-    {name: 'Marius', results: [PARTICIPATED]},
-    {name: 'Erik', results: [PARTICIPATED]},
-    {name: 'Richard', results: [NOT_PARTICIPATED]},
-    {name: 'Lars Fredrik', results: [PARTICIPATED]},
-    {name: 'Fredrik B', results: [4]},
-    {name: 'Fredrik S', results: [NOT_PARTICIPATED]},
-    {name: 'Vetle', results: [PARTICIPATED]},
-    {name: 'Ken', results: [NOT_PARTICIPATED]},
-    {name: 'Seán', results: [2]},
-    {name: 'Lars Olav', results: [1]},
-    {name: 'Terje', results: [NOT_PARTICIPATED]},
-    {name: 'Jan Tore', results: [3]},
-    {name: 'Jarle', results: [PARTICIPATED]},
-    {name: 'Ann Katrin', results: [NOT_PARTICIPATED]},
-    {name: 'Kristoffer', results: [NOT_PARTICIPATED]},
+    {name: 'Ole Tommy', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Ivar', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Jan Erik', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Herman', results: [NOT_PARTICIPATED, PARTICIPATED]},
+    {name: 'Ole-André', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Tor Eric', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Håken', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Gustav', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Erlend', results: [NOT_PARTICIPATED, PARTICIPATED]},
+    {name: 'Marius', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Erik', results: [PARTICIPATED, NOT_PARTICIPATED]},
+    {name: 'Richard', results: [NOT_PARTICIPATED, 3]},
+    {name: 'Lars Fredrik', results: [PARTICIPATED, NOT_PARTICIPATED]},
+    {name: 'Fredrik B', results: [4, PARTICIPATED]},
+    {name: 'Fredrik S', results: [NOT_PARTICIPATED, 4]},
+    {name: 'Vetle', results: [PARTICIPATED, 2]},
+    {name: 'Ken', results: [NOT_PARTICIPATED, PARTICIPATED]},
+    {name: 'Seán', results: [2, PARTICIPATED]},
+    {name: 'Lars Olav', results: [1, PARTICIPATED]},
+    {name: 'Terje', results: [NOT_PARTICIPATED, PARTICIPATED]},
+    {name: 'Jan Tore', results: [3, PARTICIPATED]},
+    {name: 'Jarle', results: [PARTICIPATED, PARTICIPATED]},
+    {name: 'Ann Katrin', results: [NOT_PARTICIPATED, 1]},
+    {name: 'Kristoffer', results: [NOT_PARTICIPATED, NOT_PARTICIPATED]},
 ];
 
 var participantsTemplate = _.template(
@@ -34,6 +34,7 @@ var participantsTemplate = _.template(
 
 var rounds = [
     {head: "Lynsjakk", points: [500, 400, 300, 200], participationScore: 100},
+    {head: "Darts", points: [500, 400, 300, 200], participationScore: 100},
 ];
 
 
