8979342eb7408a0a67a387fbf1f9c57d6eeb2254
Erik Salhus <erik.salhus@gmail.com>
2020-02-09 23:26:12 +0100

    fix error

--- next/src/ansatte-med-assets.js
+++ next/src/ansatte-med-assets.js
@@ -75,6 +75,7 @@ import Cards from './images/cards.jpeg';
 import Music from './images/music_lover.jpeg';
 import Yoga from './images/yoga.jpeg';
 import ansatte from './ansatte';
+import { showAvailableConsultantsFirst } from './utils';
 
 export const ansatteMedAssets = {
     toreric: {
