a037fb85d10cc176458d97c6bd90e274f126de39
Herman Crawfurd Svensen <h@herms.no>
2020-02-14 15:02:51 +0100

    Styling av kundereferanse.

--- src/components/EmployeeTestimonial.jsx
+++ src/components/EmployeeTestimonial.jsx
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react';
-import './EmployeeTestimonial.module.less';
-import { mdiFormatQuoteOpen } from '@mdi/js';
-import Icon from '@mdi/react';
+import './EmployeeTestimonial.less';
 
 const EmployeeTestimonial = ({ testimonial, person, position, customer }) => (
   <blockquote className="employee-testimonial">
-    <p>{testimonial}</p>
-    <p className="employee-testimonial-customer">{person}, {position} i {customer}</p>
+    <p className="employee-testimonial-content">{testimonial}</p>
+    <p className="employee-testimonial-customer">- {person}, {position} i {customer}</p>
   </blockquote>
 );
 
